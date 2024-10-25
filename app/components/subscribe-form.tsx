26ac0a3233dce2b538a011bd705745ccd34d2481
Joss <78896064+jossTripoli@users.noreply.github.com>
2024-10-25 13:42:49 -0400

    lander done

---
+++ app/components/subscribe-form.tsx
@@ -0,0 +1,41 @@
+function SubscribeForm() {
+  const newsletterheading = "Notify me when it's ready";
+  const hideSubscribeForm = false;
+
+  return (
+    <>
+    { hideSubscribeForm === false ? (
+          <section className="text-center lg:m-7 mt-10 w-5/6 p-3">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-slate-800 dark:text-slate-100  font-light text-lg leading-6"
+                >
+                  {newsletterheading}
+                </label>
+                <div className="mt-4 flex-col flex lg:flex md:flex-row text-lg">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Email address"
+                    autoComplete="email"
+                    required
+                    className="block text-lg w-full placeholder:text-gray-500 pl-[10px] focus:outline-none border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6 mr-0 rounded-none p-2"
+                  />
+                  <button className="ml-0 bg-pear sm:w-auto border-darkpear dark:border-slate-100	rounded-none mt-2 md:mt-0	p-2 border-2 dark:text-white hover:bg-skyblue">
+                    Subscribe
+                  </button>
+                </div>
+              </div>
+            </form>
+          </section>
+        ) : (
+          ""
+        )}
+    </>
+  )
+}
+
+export default SubscribeForm
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,84 @@
+'use client';
+
 import Image from "next/image";
+import SubscribeForm from "./components/subscribe-form";
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex items-center justify-center min-h-screen">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <div className="flex flex-col items-center">
+        <div className="bg-white p-6 text-center rounded-lg shadow-l flex flex-col items-center">
+          <Image 
+            src="https://dart-academy-public-resources.s3.us-east-1.amazonaws.com/logo/logo.svg" 
+            alt="DART Academy Logo" 
+            width={350} 
+            height={350} 
+            className="mb-8"
+          />
+          <div className="w-full">
+            <h1 className="text-xl font-bold text-sapphire mb-4 text-left">Coming Soon</h1>
+            <p className="text-lg text-richBlack mb-6 text-left">Learn how to protect yourself from scams.</p>
+          </div>
+
+          
+          <SubscribeForm />
+
+          {/* <form className="relative z-10 mx-10 lg:max-w-xl lg:mx-auto border-blue-500">
+            <input
+              type="text"
+              placeholder="name@email.com"
+              className="w-full text-2xl font-light text-gray-900 placeholder-gray-500 py-5 pl-5 pr-36 lg:pr-44 rounded-xl"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <button className="absolute top-1 right-1 bottom-1 px-4 lg:px-10 text-xl font-semibold bg-pear text-black rounded-xl transition ease-in-out duration-500 hover:bg-red-500">
+              Notify me
+            </button>
+          </form> */}
+
+
+          {/* <div className="flex space-x-4">
+            <button className="px-8 py-2 text-xl rounded-full bg-pear text-richBlack hover:bg-skyblue transition">
+              Notify Me
+            </button>
+            <a href="https://dartcollective.net/dart-academy/" target="_blank" rel="noreferrer">
+              <button className="px-8 py-2 text-xl rounded-full bg-pear text-richBlack hover:bg-skyblue transition">
+                Learn more
+              </button>
+            </a>
+          </div> */}
+        </div>
+
+
+        <div className="flex mt-8 space-x-4">
+          <a 
+            href="https://www.facebook.com" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="w-12 h-12 flex items-center justify-center rounded-full bg-white text-sapphire shadow-lg hover:bg-sapphire hover:text-white transition-colors"
           >
-            Read our docs
+            <FaFacebookF size={25} />
+          </a>
+          <a 
+            href="https://www.instagram.com" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="w-12 h-12 flex items-center justify-center rounded-full bg-white text-sapphire shadow-lg hover:bg-sapphire hover:text-white transition-colors"
+          >
+            <FaInstagram size={25} />
+          </a>
+          <a 
+            href="https://www.linkedin.com" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="w-12 h-12 flex items-center justify-center rounded-full bg-white text-sapphire shadow-lg hover:bg-sapphire hover:text-white transition-colors"
+          >
+            <FaLinkedinIn size={25} />
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+      </div>
     </div>
   );
 }
